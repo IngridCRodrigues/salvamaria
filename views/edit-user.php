6628eb44c608bf31b30e5dd1b7015b5b68a038f2
Ingrid Rodrigues <ingridcruz@infojr.com.br>
2017-06-04 04:41:58 -0300

    alteracoes de edicao de usuarios

---
+++ views/edit-user.php
@@ -0,0 +1,18 @@
+<?php $context = "users"; ?>
+
+<?php require_once('../heart/pulse.php'); ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<?php include('includes/head.inc'); ?>
+	<title>Novo usuário</title>
+</head>
+<body>
+	<?php include('includes/header-admin.inc'); ?>
+<section>
+<?php include('includes/users.inc'); ?>
+	
+</section>
+</body>
+</html>
