099d4573e17f347bf589e9b1edc32d11294d944c
Ingrid Rodrigues <ingridcruz@infojr.com.br>
2017-06-04 01:51:12 -0300

    Inicia crud de usuários/mulheres

---
+++ views/users.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <?php include('includes/head.inc'); ?>
+    <title>Mulheres</title>
+</head>
+<body class="panel">
+    <?php include('includes/header-admin.inc'); ?>
+<section>
+    <table class="gt-table striped hovered">
+        <thead>
+            <tr>
+                <th>Nome</th>
+                <th>Cidade</th>
+                <th>Ações</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Maria</td>
+                <td>Salvador</td>
+                <td>
+                    <a href="">Editar</a>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</section>
+</body>
+</html>
