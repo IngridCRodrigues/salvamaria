cf4d968d6f29973d415965b266e90da16a1caf45
Ingrid Rodrigues <ingridcruz@infojr.com.br>
2017-06-04 04:26:51 -0300

    create funcionando

--- models/user.php
+++ models/user.php
@@ -2,6 +2,9 @@
     require_once('../heart/model/base.php');
 
     class User extends \Model\Base {
-        public $fillable = ['picture', 'name', 'email', 'level'];
+        public $fillable = ['processo', 'nome', 'rua', 'bairro', 'numero', 'cidade', 'complemento', 'endereco_visita', 'ponto_referencia', 'telefone', 'turno_visita', 'estado_civil', 'filhos', 'filhos_com_agressor', 'escolaridade', 'etnia', 'idade', 'religiao', 'profissao', 'trabalha', 'ultimo_emprego', 'local_trabalho', 'renda', 'responsavel_sustento' ];
     }
 ?>
+
+
+<!-- 'rua', 'bairro', 'numero', 'cidade', 'complemento', 'endereco_visita', 'ponto_referencia', 'telefone', 'turno_visita', 'estado_civil', 'filhos', 'filhos_com_agressor', 'escolaridade', 'etnia', 'idade', 'religiao', 'profissao', 'trabalha', 'ultimo_emprego', 'local_trabalho', 'renda', 'responsavel_sustento' -->
