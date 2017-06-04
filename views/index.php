fa0f7b01dabce12ceb64246ea5ba87b8719f5562
Ingrid Rodrigues <ingridcruz@infojr.com.br>
2017-06-04 01:10:51 -0300

    Inicia projeto

---
+++ views/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Minha página</title>
+    <link rel="stylesheet" href="css/gaintime.min.css" media ="screen" title="no title">
+  </head>
+  <body>
+    <h1>Fique focado, e vai vencer no final.</h1>
+    <script type="text/javascript" src="js/gaintime.min.js"></script>
+  </body>
+</html>
+        
---
+++ views/new-user.php
@@ -0,0 +1,17 @@
+<?php require_once('heart/pulse.php'); ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Novo usuário</title>
+</head>
+<body>
+<form action="../controllers/user_controller.php" method="POST">
+<label for="nome">Nome:</label>
+<input id="nome" type="text" name="nome">
+<label for="endereco">Endereco:</label>
+<input id="endereco" type="text" name="endereco">
+	
+</form>
+</body>
+</html>
